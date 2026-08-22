a6466d2b0e66db97d72faa4fd353f0a95a181948
Nick Wells <wells.nicholas@gmail.com>
2026-08-22 12:56:49 +0100

    feature(mkProgDir): remove the set/forceExitStatus funcs
    These are removed from the Go program template. The thinking is that they
    don't add any value over just setting the exit status directly.

--- mkProgDir/_templates/Go/prog.go
+++ mkProgDir/_templates/Go/prog.go
@@ -21,20 +21,6 @@ func newProg() *prog {
 	}
 }
 
-// setExitStatus sets the exit status to the new value. It will not do this
-// if the exit status has already been set to a non-zero value.
-func (prog *prog) setExitStatus(es int) {
-	if prog.exitStatus == 0 {
-		prog.exitStatus = es
-	}
-}
-
-// forceExitStatus sets the exit status to the new value. It will do this
-// regardless of the existing exit status value.
-func (prog *prog) forceExitStatus(es int) {
-	prog.exitStatus = es
-}
-
 // run is the starting point for the program, it should be called from main()
 // after the command-line parameters have been parsed. Use the setExitStatus
 // method to record the exit status and then main can exit with that status.
